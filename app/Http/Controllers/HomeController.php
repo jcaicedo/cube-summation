f90f55168260db23e1183350e17ee29d688333e2
caicedo.julio <caicedo.julio@gmail.com>
2017-01-01 15:41:20 -0400

    refactor de procesamiento de cubo y vista

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,60 +11,183 @@ class HomeController extends Controller
 
   public function postCube(Request $request){
 
+
+    $error=[];
+    $data =[];
     $results=[];
+    $n=0;
+
     if ($request->hasFile('file-input')) {
       $file = $request->file('file-input')->getPathName();
 
       $fh = fopen($file,'r');
-      $test_cases = trim(fgets($fh)) ;
-      $n=0;
-      while($n<$test_cases){
-        echo "test: ".$n."<br>";
-        $line = $this->clearLine(fgets($fh));
-        $size_matriz = $line[0];
-        $number_operations=$line[1];
-        $matriz = $this->createMatriz($size_matriz);
+      $test_cases = (int)trim(fgets($fh)) ; //numero de casos
+      if ((1<=$test_cases)&&($test_cases<=50)) {
+        while($n<$test_cases){
 
-        echo "size_matriz: ".$size_matriz." number_operations: ".$number_operations."<br>";
-        for ($i = 0; $i < $number_operations ; $i++) {
           $line = $this->clearLine(fgets($fh));
-          switch ($line[0]) {
-            case 'UPDATE':
-            $matriz[$line[1]][$line[2]][$line[3]]=(int)$line[4];
-            echo "update: ".$matriz[$line[1]][$line[2]][$line[3]]."<br>";
-            break;
-            case 'QUERY':
-            $results[]=$this->cubeSummation($line,$matriz);
-            break;
+          if (!feof($fh)) {
+            $size_matriz = (int)$line[0]; //tamaño de la matriz
+            $number_operations=(int)$line[1]; //numero de operaciones
+            $matriz = $this->createMatriz($size_matriz);
 
-            default:
-            # code...
+            if ((1<=$size_matriz)&&($size_matriz<=100)) {
+              if((1<=$number_operations)&&($number_operations<=1000)){
+
+                for ($r=0; $r < $number_operations ; $r++) {
+                  $line = $this->clearLine(fgets($fh));  //clear line
+
+
+                  switch ($line[0]) {
+
+                    case 'UPDATE':
+                    if ((1<=$line[1])&&(1<=$line[2])&&(1<=$line[3])&&($line[1]<=$size_matriz)&&($line[2]<=$size_matriz)&&($line[3]<=$size_matriz)&&(pow(-10, 9)<=$line[4])&&($line[4]<=pow(10, 9))) {
+                      $matriz[$line[1]][$line[2]][$line[3]]=(int)$line[4];
+                    }else{
+                      $error[]="Error en UPDATE ".$r." de la iteración: ".($n+1).", donde 1 <= x,y,z <= N y -10^9 <= W <= 10^9 ";
+                    }
+
+                    break;
+
+                    case 'QUERY':
+                    if (!(1<=(int)$line[1]) && !((int)$line[1]<(int)$line[4]) && !((int)$line[4]<=$size_matriz)) {
+                      $error[] = "Los valores para x en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=x1<=x2<=N";
+                    }else{
+                      if (!(1<=(int)$line[2]) && !((int)$line[2]<(int)$line[5]) && !((int)$line[5]<=$size_matriz)) {
+                        $error[] = "Los valores para y en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=y1<=y2<=N";
+                      }else{
+                        if (!(1<=(int)$line[3]) && !((int)$line[3]<(int)$line[6]) && !((int)$line[6]<=$size_matriz)) {
+                          $error[] = "Los valores para z en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=z1<=z2<=N";
+                        }else{
+                          $results[]=$this->cubeSummation($line,$matriz);
+                        }
+                      }
+                    }
+
+
+                    break;
+
+                  }
+                }
+
+              }else{
+                $error[]="El valor de M debe ser 1<=M<=1000 en la iteracion #".($n+1);
+              }
+            }else{
+              $error[]="El valor de N debe ser 1<=N<=100 en la iteracion #".($n+1);
+            }
+            $n++;
+          }else{
+            $error[]="No concuerda el valor de T con la cantidad de casos";
             break;
           }
-        }
 
-        $n++;
+        }
+      }else{
+        $error[]="El valor de T debe ser 1<=T<=50";
       }
 
       fclose($fh);
-      dd($results);
-      // dd($request->file('file-input')->getPathName());
+      $data['errors']=$error;
+      $data['results']=$results;
+      return $data;
     }else{
       dd('no');
     }
   }
 
 
-//Descomposicion de la linea de archivo
+  public function processFile($file){
+    $error=[];
+    $data = [];
+    $fh = fopen($file,'r');
+    $data =[];
+    $results=[];
+    $n=0;
+
+    $test_cases = (int)trim(fgets($fh)) ; //numero de casos
+    if ((1<=$test_cases)&&($test_cases<=50)) {
+      while($n<$test_cases){
+
+        $line = $this->clearLine(fgets($fh));
+        if (!feof($fh)) {
+          $size_matriz = (int)$line[0]; //tamaño de la matriz
+          $number_operations=(int)$line[1]; //numero de operaciones
+          $matriz = $this->createMatriz($size_matriz);
+
+          if ((1<=$size_matriz)&&($size_matriz<=100)) {
+            if((1<=$number_operations)&&($number_operations<=1000)){
+
+              for ($r=0; $r < $number_operations ; $r++) {
+                $line = $this->clearLine(fgets($fh));  //clear line
+
+
+                switch ($line[0]) {
+
+                  case 'UPDATE':
+                  if ((1<=$line[1])&&(1<=$line[2])&&(1<=$line[3])&&($line[1]<=$size_matriz)&&($line[2]<=$size_matriz)&&($line[3]<=$size_matriz)&&(pow(-10, 9)<=$line[4])&&($line[4]<=pow(10, 9))) {
+                    $matriz[$line[1]][$line[2]][$line[3]]=(int)$line[4];
+                  }else{
+                    $error[]="Error en UPDATE ".$r." de la iteración: ".($n+1).", donde 1 <= x,y,z <= N y -10^9 <= W <= 10^9 ";
+                  }
+
+                  break;
+
+                  case 'QUERY':
+                  if (!(1<=(int)$line[1]) && !((int)$line[1]<(int)$line[4]) && !((int)$line[4]<=$size_matriz)) {
+                    $error[] = "Los valores para x en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=x1<=x2<=N";
+                  }else{
+                    if (!(1<=(int)$line[2]) && !((int)$line[2]<(int)$line[5]) && !((int)$line[5]<=$size_matriz)) {
+                      $error[] = "Los valores para y en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=y1<=y2<=N";
+                    }else{
+                      if (!(1<=(int)$line[3]) && !((int)$line[3]<(int)$line[6]) && !((int)$line[6]<=$size_matriz)) {
+                        $error[] = "Los valores para z en la iteracion ".($n+1)." de la operación ".$line[0]." debe cumplie con 1<=z1<=z2<=N";
+                      }else{
+                        $results[]=$this->cubeSummation($line,$matriz);
+                      }
+                    }
+                  }
+
+
+                  break;
+
+                }
+              }
+
+            }else{
+              $error[]="El valor de M debe ser 1<=M<=1000 en la iteracion #".($n+1);
+            }
+          }else{
+            $error[]="El valor de N debe ser 1<=N<=100 en la iteracion #".($n+1);
+          }
+          $n++;
+        }else{
+          $error[]="No concuerda el valor de T con la cantidad de casos";
+          break;
+        }
+
+      }
+    }else{
+      $error[]="El valor de T debe ser 1<=T<=50";
+    }
+
+    fclose($fh);
+    $data['errors']=$error;
+    $data['results']=$results;
+    dd( $data);
+  }
+
+  //Descomposicion de la linea de archivo
   public function clearLine($line){
     $line = explode(" ", $line);
     foreach ($line as $key => $value) {
       $line[$key]=trim($value);
+
     }
     return $line;
   }
 
-//creacion de nueva matriz
+  //creacion de nueva matriz
   public function createMatriz($size){
     $matriz = [];
     for ($x=1; $x < $size+1 ; $x++) {
@@ -77,7 +200,7 @@ class HomeController extends Controller
     return $matriz;
   }
 
-//sumatoria de la matriz de matriz
+  //sumatoria de la matriz de matriz
   public function cubeSummation($line,$matriz=[]){
     $summation = 0;
     for ($x = $line[1]; $x < $line[4]+1; $x++) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,68 @@
       </div>
     </div>
     <div class="col-md-12">
-      <form class="" action="/cube" method="post" enctype="multipart/form-data" >
+      <form id="form-file" enctype="multipart/form-data" >
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group col-md-12">
           <label for="interaction">Cargue archivo de entrada</label>
           <input type="file" name="file-input" class="form-control">
         </div>
         <div class="col-md-12">
-          <button class="btn btn-default">Calcular</button>
+          <button type="button" class="btn btn-default">Calcular</button>
         </div>
 
       </form>
     </div>
   </div>
+
+  <div class="container" style="margin-top:36px;">
+    <div class="row" id="results">
+
+    </div>
+  </div>
+
 @endsection
+
+@push('scripts')
+  <script type="text/javascript">
+  $(document).ready(function(){
+    $(".btn-default").on("click",function(){
+      var formData = new FormData($("#form-file"));
+      $.ajaxSetup(
+        {
+          headers:
+          {
+            'X-CSRF-Token': $('input[name="_token"]').val()
+          }
+        });
+        $.ajax({
+          url: "cube",
+          type: "post",
+          dataType: "html",
+          data: new FormData($("#form-file")[0]),
+          cache: false,
+          contentType: false,
+          processData: false
+        }).done(function(data){
+
+          obj=jQuery.parseJSON( data );
+
+          if(obj.errors.length==0){
+            $("#results").html("");
+            $.each(obj.results, function(i, item) {
+              $("#results").append("<div class='col-md-8 col-md-offset-2 text-center'><p>"+obj.results[i]+"</p></div>");
+            })
+
+
+          }else{
+            $("#results").html("");
+            $.each(obj.errors, function(i, item) {
+              $("#results").append("<div class='col-md-8 col-md-offset-2 text-center'><p>"+obj.errors[i]+"</p></div>");
+            })
+          }
+        });
+      });
+      ;
+    });
+    </script>
+  @endpush
